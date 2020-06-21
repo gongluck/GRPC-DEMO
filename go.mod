6d401d72e219e9ee26e036692f2b24c0ae90edc5
gongluck <1039994845@qq.com>
2020-06-21 12:55:16 +0800

    tutorial

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module rpc-demo
 
 go 1.14
+
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/jergoo/go-grpc-example v0.0.0-20191112085010-4cd5156242f3 // indirect
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+	google.golang.org/grpc v1.27.0
+	google.golang.org/protobuf v1.24.0
+)
---
+++ tutorial/client/main.go
@@ -0,0 +1,39 @@
+/*
+ * @Author: gongluck
+ * @Date: 2020-06-21 12:49:00
+ * @Last Modified by: gongluck
+ * @Last Modified time: 2020-06-21 12:49:27
+ */
+
+package main
+
+import (
+	pb "rpc-demo/tutorial" // 引入proto包
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
+)
+
+const (
+	// Address gRPC服务地址
+	Address = "127.0.0.1:50052"
+)
+
+func main() {
+	// 连接
+	conn, err := grpc.Dial(Address, grpc.WithInsecure())
+	if err != nil {
+		grpclog.Fatalln(err)
+	}
+	defer conn.Close()
+	// 初始化客户端
+	c := pb.NewHelloClient(conn)
+	// 调用方法
+	req := &pb.HelloRequest{Name: "gRPC"}
+	res, err := c.SayHello(context.Background(), req)
+	if err != nil {
+		grpclog.Fatalln(err)
+	}
+	grpclog.Println(res.Message)
+}
